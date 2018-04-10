a2be5e443672835441629891633cb5420cda01f4
anstettd <daniel_anstett@yahoo.ca>
2018-04-10 07:55:53 -0700

    Updates from last night

--- Scripts/pilot.R
+++ Scripts/pilot.R
@@ -29,6 +29,7 @@ cor.test(clim$CMD, clim$MAT)
 cor.test(clim$MAT, clim$MAP)
 # they are all highly correlated, not much independent information across them
 # so its probably best to use CMD
+
 ########
 ###CMD Selected as the predictor enviornemntal variable 
 ########
@@ -37,7 +38,7 @@ lm1.3way<-lm(Flower_Date~Drought*Treatment*CMD)
 a1.3way<-Anova(lm1.3way, type=3)
 a1.3way # no support for 3-way interaction
 
-lm1.2ways < -lm(Flower_Date~Drought + Treatment + CMD + Drought*Treatment + Treatment*CMD + Drought*CMD)
+lm1.2ways <-lm(Flower_Date~Drought + Treatment + CMD + Drought*Treatment + Treatment*CMD + Drought*CMD)
 a1.2ways <- Anova(lm1.2ways, type=3)
 a1.2ways # significant Drought x CMD interaction
 visreg(lm1.2ways, xvar="CMD", by="Drought") # rapid evolution of earlier flowering only in wet sites
@@ -69,6 +70,8 @@ visreg(lm2.2way, xvar="Treatment")
 lm2.mains <- lm(Flower~Drought+Treatment+CMD)
 a2.mains <- Anova(lm2.mains, type=3)
 a2.mains
+
+
 
 # Biomass
 lm3.3way <- lm(Biomass~Drought*Treatment*CMD)
