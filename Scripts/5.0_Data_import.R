5a4b104323f9dba85ca3350f84707ddedea09587
anstettd <daniel_anstett@yahoo.ca>
2019-07-29 10:45:02 -0700

    Clean up

--- Scripts/5.0_Data_import.R
+++ Scripts/5.0_Data_import.R
@@ -18,7 +18,6 @@ library(glmmTMB)
 
 ### Data prep
 Y <- read.csv("Data/drought1.csv", header=T)
-
 #Add in flowering time data
 flower1<-read.csv("Data/flower_date_ver2.csv", header=T)
 colnames(flower1)[1]<-"Order1"
@@ -27,7 +26,6 @@ flower1[,6]<-flower1[,5]-101
 colnames(flower1)[6]<-"Experiment_Date"
 #y1<-left_join(Y,flower1,by=c("Order"="Order1"))
 y1<-left_join(Y,flower1,by=c("Order"="Order1", "Family"="Family", "Block"="Block", "Drought"="Treatment"))
-
 #Add in other physical traits
 rapid<-read.csv("Data/rapid.csv", header=T)
 y2<-left_join(y1,rapid, by=c("Order"="Order2", "Family"="Family", "Block"="Block", "Drought"="Treatment"))
@@ -36,27 +34,22 @@ y2[,19]<-y2[,18]/y2[,17] #better habit to do this by col names instead of col #s
 y2[,20]<-y2[,17]/y2[,16]
 colnames(y2)[19]<-"SLA"
 colnames(y2)[20]<-"Water_Content"
-
 #Make a categorical site variable that is ordered by latitude
 wna1 <- read_csv("Climate/timeseries_lat_2010-2016.csv") %>%
   select(ID_Year1,Latitude,Longitude) %>% #,Elevation,MAT,MAP,CMD 
   separate(ID_Year1, into = c("Site", "Year"), sep = "_")
 wna1$Site <- as.factor(wna1$Site)
 wna1$Year <- as.numeric(wna1$Year)
-
 y3 <- left_join(y2, wna1, by=c("Site", "Year"))
-
 y3 <- y3 %>% mutate(Site.Lat = paste(round(Latitude,1), Site, sep="_"))  
-attach(y3)
 
-# Bring in point measures data set (Mimulus 2018)
+
+###### Bring in photosythesis point measures data set (Mimulus 2018) ##
 point_measure<-read.csv("Data/mimulusjuly2018.csv", header=T)
 # Make vairable that sumarizes each set of three within plant replicates
 point_measure <- point_measure %>% mutate(ID.B.D = paste(Plant.ID, Block, Treatment, sep="_"))  
-
 #Take average of 3 reps per A and gs
 point_1 <- data.frame()
-
 # take 3 obs and turn into one for photosythesis point measures
 U_ID3<-unique(point_measure$ID.B.D) # Lengh of vector of unique ID.B.D
 for (i in 1:length(U_ID3)){ #establish for loop going from 1 to the length of unique ID vector (U_ID3)
@@ -69,20 +62,12 @@ for (i in 1:length(U_ID3)){ #establish for loop going from 1 to the length of un
   point_1[i,4]<-unique(point.temp$Block)
   point_1[i,5]<-unique(point.temp$Treatment)
   point_1[i,6]<-unique(point.temp$ID.B.D)
-  
   point_1[i,7]<-temp.mean.gs
   point_1[i,8]<-temp.mean.A
 }
-colnames(point_1)<-c("Family", "Site", "Year", "Block", "Drought", "ID.B.D", "Stomatal_Conductance","Assimilation")
-
+colnames(point_1)<-c("Family", "Site.1", "Year.1", "Block", "Drought", "ID.B.D", "Stomatal_Conductance","Assimilation")
 #Join point measurements to full data set
 y3 <- left_join(y3, point_1, by=c("Family", "Block", "Drought"))
-
-
-
-
-
-
 
 
 ####### Data Import Climate and Anomaly #########  
@@ -112,26 +97,18 @@ wna_all <- left_join(wna2, wna, by="Site") %>%
          MAP.weath.scaled = as.vector(scale(MAP.weath)),
          CMD.anom.scaled = as.vector(scale(CMD.anom)),
          MAT.anom.scaled = as.vector(scale(MAT.anom)),
-         MAP.anom.scaled = as.vector(scale(MAP.anom)),)
+         MAP.anom.scaled = as.vector(scale(MAP.anom)))
 
 # join all data into one frame
-y4 <- left_join(y3, wna_all, by=c("Site.x"="Site", "Year.x"="Year"))
+y3 <- left_join(y3, wna_all, by=c("Site"="Site", "Year"="Year"))
 
 #Scale 
-y4 <- y4 %>% mutate(Experiment_Date.scaled = scale(Experiment_Date),
+y3 <- y3 %>% mutate(Experiment_Date.scaled = scale(Experiment_Date),
                     SLA.scaled = scale(SLA),
                     Water_Content.scaled = scale(Water_Content),
                     Structure.scaled = scale (Structure),
                     Wilted.scaled = scale(Wilted),
                     Stomatal_Conductance.s = scale(Stomatal_Conductance),
-                    Assimilation.s = (Assimilation))
+                    Assimilation.s = scale(Assimilation))
 
-
-
-
-
-
-
-
-
-
+write.csv(y3,'Data/y3.csv') #Export file
