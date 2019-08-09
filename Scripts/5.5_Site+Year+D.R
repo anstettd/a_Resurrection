af30a7bc2ae4ed53995858e93269eb1cb2dd71ba
anstettd <daniel_anstett@yahoo.ca>
2019-08-09 15:40:11 -0700

    5.5 poly

--- Scripts/5.5_Site+Year+D.R
+++ Scripts/5.5_Site+Year+D.R
@@ -23,7 +23,8 @@ fullmod.exp <- lmer(Experiment_Date ~ Site.Lat*Year*Drought + (1|Family) + (1|Bl
 no3way.exp <- lmer(Experiment_Date ~ Site.Lat*Drought + Drought*Year + Site.Lat*Year+ (1|Family) + (1|Block), data=y3)
 lrtest(fullmod.exp, no3way.exp) #3-way intraction significant, 3-way has a larger LogLik value. Retain 3-way.
 Anova(fullmod.exp, type = 3) # Drought * Site interaction, Site * Year interaction 
-#Simple graphing    
+
+#Simple graphing 3-way
 visreg_flower_D<-visreg(fullmod.exp, xvar="Year", by="Site.Lat", cond=list(Drought="D"),jitter=TRUE, gg=TRUE)+
   facet_wrap(.~Site.Lat)+ # Make 12 pannel gg plot with visreg for Drought flower time data
   theme(panel.background=element_rect(fill="white"), strip.background=element_rect(fill="white"), #theme modifications
@@ -39,6 +40,34 @@ visreg_flower_W<-visreg(fullmod.exp, xvar="Year", by="Site.Lat", cond=list(Droug
         axis.text.x=element_text(angle=45,hjust=1))
 visreg_flower_W # Show 12 site plot for Wet Treatment
 #See "5.6_Stie*Year*Drought_graphs.R" for graphs with W and D on same plot
+
+
+
+#Poly 3-way
+fullmod.poly.exp <- lmer(Experiment_Date ~ Site.Lat*poly(Year,2)*Drought + (1|Family) + (1|Block), data=y3) #3way interaction model
+lrtest(fullmod.exp, fullmod.poly.exp) #3-way poly supported. Retain:fullmod.poly.exp
+#poly drop 3way
+no3way.poly.exp <- lmer(Experiment_Date ~ Site.Lat*Drought + Drought*poly(Year,2) + Site.Lat*poly(Year,2)+ (1|Family) + (1|Block), data=y3)
+lrtest(fullmod.poly.exp,no3way.poly.exp) #3-way poly supported
+Anova(fullmod.poly.exp)
+
+#Simple graphing 3-way poly
+visreg_flower_D<-visreg(fullmod.poly.exp, xvar="Year", by="Site.Lat", cond=list(Drought="D"),jitter=TRUE, gg=TRUE)+
+  facet_wrap(.~Site.Lat)+ # Make 12 pannel gg plot with visreg for Drought flower time data
+  theme(panel.background=element_rect(fill="white"), strip.background=element_rect(fill="white"), #theme modifications
+        panel.grid.major=element_line(colour="grey90"),
+        panel.grid.minor=element_line(colour="grey90"), 
+        axis.text.x=element_text(angle=45,hjust=1))
+visreg_flower_D # Show 12 site plot for Drought Treatment
+visreg_flower_W<-visreg(fullmod.poly.exp, xvar="Year", by="Site.Lat", cond=list(Drought="W"),jitter=TRUE, gg=TRUE)+ 
+  facet_wrap(.~Site.Lat)+ # Make 12 pannel gg plot with visreg for Wet flower time data
+  theme(panel.background=element_rect(fill="white"), strip.background=element_rect(fill="white"),#theme modifications
+        panel.grid.major=element_line(colour="grey90"),
+        panel.grid.minor=element_line(colour="grey90"), 
+        axis.text.x=element_text(angle=45,hjust=1))
+visreg_flower_W # Show 12 site plot for Wet Treatment
+#See "5.6_Stie*Year*Drought_graphs.R" for graphs with W and D on same plot
+
 
 ##### Water_Content ####
 fullmod.wc <- lmer(Water_Content ~ Site.Lat*Year*Drought + (1|Family) + (1|Block), data=y3)
@@ -64,6 +93,7 @@ lrtest(noYear.wc,siteXdrought.wc) # Main effect model with site and drought is b
 year.rad.wc <- lmer(Water_Content ~ Site.Lat + Drought + (1|Year) + (1|Family) + (1|Block), data=y3)
 lrtest(noYear.wc,year.rad.wc) # Main effect model with site and drought is best.
 Anova(noYear.wc, type = 3) # Site and drought main effect.
+
 #plotting water content vs time per Drought
 visreg_wc_D<-visreg(fullmod.wc, xvar="Year", by="Site.Lat", cond=list(Drought="D"),jitter=TRUE, gg=TRUE)+
   facet_wrap(.~Site.Lat)+
@@ -80,6 +110,34 @@ visreg_wc_W<-visreg(fullmod.wc, xvar="Year", by="Site.Lat", cond=list(Drought="W
         axis.text.x=element_text(angle=45,hjust=1))
 visreg_wc_W
 
+#Poly 3-way
+fullmod.poly.wc <- lmer(Water_Content ~ Site.Lat*poly(Year,2)*Drought + (1|Family) + (1|Block), 
+                        control=lmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=100000)),data=y3)
+# drop 3way
+no3way.poly.wc <- lmer(Water_Content ~ Site.Lat*Drought + Drought*poly(Year,2) + Site.Lat*poly(Year,2) + (1|Family) + (1|Block), data=y3)
+lrtest(fullmod.poly.wc, no3way.poly.wc) # keep 3-way, keep fullmod.poly.wc 
+#compare poly to non-poly-3way
+lrtest(fullmod.poly.wc, fullmod.wc) #poly 3-way is better
+Anova(fullmod.poly.wc)
+
+#Poly plotting water content vs time per Drought
+visreg_wc_D<-visreg(fullmod.poly.wc, xvar="Year", by="Site.Lat", cond=list(Drought="D"),jitter=TRUE, gg=TRUE)+
+  facet_wrap(.~Site.Lat)+
+  theme(panel.background=element_rect(fill="white"), strip.background=element_rect(fill="white"),
+        panel.grid.major=element_line(colour="grey90"),
+        panel.grid.minor=element_line(colour="grey90"), 
+        axis.text.x=element_text(angle=45,hjust=1))
+visreg_wc_D
+visreg_wc_W<-visreg(fullmod.poly.wc, xvar="Year", by="Site.Lat", cond=list(Drought="W"),jitter=TRUE, gg=TRUE)+
+  facet_wrap(.~Site.Lat)+
+  theme(panel.background=element_rect(fill="white"), strip.background=element_rect(fill="white"),
+        panel.grid.major=element_line(colour="grey90"),
+        panel.grid.minor=element_line(colour="grey90"), 
+        axis.text.x=element_text(angle=45,hjust=1))
+visreg_wc_W
+
+
+
 
 ##### SLA ####
 fullmod.SLA <- lmer(SLA ~ Site.Lat*Year*Drought + (1|Family) + (1|Block), data=y3)
@@ -88,6 +146,32 @@ no3way.SLA <- lmer(SLA ~ Site.Lat*Drought + Drought*Year + Site.Lat*Year + (1|Fa
 lrtest(fullmod.SLA, no3way.SLA) # accept 3-way model
 Anova(fullmod.SLA)
 #See "5.6_Stie*Year*Drought_graphs.R" for graphs with W and D on same plot
+
+#3-way poly
+fullmod.poly.SLA <- lmer(SLA ~ Site.Lat*poly(Year,2)*Drought + (1|Family) + (1|Block), data=y3)
+# drop 3way
+no3way.poly.SLA <- lmer(SLA ~ Site.Lat*Drought + Drought*poly(Year,2) + Site.Lat*poly(Year,2) + (1|Family) + (1|Block), data=y3)
+lrtest(fullmod.poly.SLA, no3way.poly.SLA) # accept 3-way model
+#compare poly to non-poly-3way
+lrtest(fullmod.poly.SLA, fullmod.SLA) #poly 3-way is better
+Anova(fullmod.poly.SLA)
+
+#Poly plotting water content vs time per Drought
+visreg_wc_D<-visreg(fullmod.poly.SLA, xvar="Year", by="Site.Lat", cond=list(Drought="D"),jitter=TRUE, gg=TRUE)+
+  facet_wrap(.~Site.Lat)+
+  theme(panel.background=element_rect(fill="white"), strip.background=element_rect(fill="white"),
+        panel.grid.major=element_line(colour="grey90"),
+        panel.grid.minor=element_line(colour="grey90"), 
+        axis.text.x=element_text(angle=45,hjust=1))
+visreg_wc_D
+visreg_wc_W<-visreg(fullmod.poly.SLA, xvar="Year", by="Site.Lat", cond=list(Drought="W"),jitter=TRUE, gg=TRUE)+
+  facet_wrap(.~Site.Lat)+
+  theme(panel.background=element_rect(fill="white"), strip.background=element_rect(fill="white"),
+        panel.grid.major=element_line(colour="grey90"),
+        panel.grid.minor=element_line(colour="grey90"), 
+        axis.text.x=element_text(angle=45,hjust=1))
+visreg_wc_W
+
 
 ######## Stomatal Conductance
 fullmod.gs <- lmer(Stomatal_Conductance ~ Site.Lat*Year*Drought + (1|Family) + (1|Block), data=y3)
@@ -128,6 +212,32 @@ visreg_gs_W<-visreg(fullmod.gs, xvar="Year", by="Site.Lat", cond=list(Drought="W
         axis.text.x=element_text(angle=45,hjust=1))
 visreg_gs_W
 
+#Poly 3-way
+fullmod.poly.gs <- lmer(Stomatal_Conductance ~ Site.Lat*poly(Year,2)*Drought + (1|Family) + (1|Block), data=y3)
+# drop 3way
+no3way.poly.gs <- lmer(Stomatal_Conductance ~ Site.Lat*Drought + Drought*poly(Year,2) + Site.Lat*poly(Year,2) 
+                       + (1|Family) + (1|Block), data=y3)
+lrtest(fullmod.poly.gs, no3way.poly.gs) # keep 3-way
+#compare poly to non-poly-3way
+lrtest(fullmod.poly.gs, fullmod.gs) #poly 3-way is better
+Anova(fullmod.poly.gs)
+#poly plotting stomtal conductence vs time per Drought
+visreg_gs_D<-visreg(fullmod.poly.gs, xvar="Year", by="Site.Lat", cond=list(Drought="D"),jitter=TRUE, gg=TRUE)+
+  facet_wrap(.~Site.Lat)+
+  theme(panel.background=element_rect(fill="white"), strip.background=element_rect(fill="white"),
+        panel.grid.major=element_line(colour="grey90"),
+        panel.grid.minor=element_line(colour="grey90"), 
+        axis.text.x=element_text(angle=45,hjust=1))
+visreg_gs_D
+visreg_gs_W<-visreg(fullmod.poly.gs, xvar="Year", by="Site.Lat", cond=list(Drought="W"),jitter=TRUE, gg=TRUE)+
+  facet_wrap(.~Site.Lat)+
+  theme(panel.background=element_rect(fill="white"), strip.background=element_rect(fill="white"),
+        panel.grid.major=element_line(colour="grey90"),
+        panel.grid.minor=element_line(colour="grey90"), 
+        axis.text.x=element_text(angle=45,hjust=1))
+visreg_gs_W
+
+
 ######## Assimilation
 #fullmod.A <- lmer(Assimilation ~ Site.Lat*Year*Drought + (1|Family) + (1|Block), data=y3) #convergence problem
 fullmod.Aba <- lmer(Assimilation ~ Site.Lat*Year*Drought + (1|Family) + (1|Block), 
@@ -161,6 +271,34 @@ visreg_flower_W<-visreg(fullmod.exp, xvar="Year", by="Site.Lat", cond=list(Droug
         axis.text.x=element_text(angle=45,hjust=1))
 visreg_flower_W
 
+#Poly 3-way
+fullmod.poly.A <- lmer(Assimilation ~ Site.Lat*poly(Year,2)*Drought + (1|Family) + (1|Block), data=y3)
+# drop 3way
+no3way.poly.A <- lmer(Assimilation ~ Site.Lat*Drought + Drought*poly(Year,2) + Site.Lat*poly(Year,2) 
+                       + (1|Family) + (1|Block), data=y3)
+lrtest(fullmod.poly.A, no3way.poly.A) # keep 3-way
+#compare poly to non-poly-3way
+lrtest(fullmod.poly.A, fullmod.Aba) #poly 3-way is better
+Anova(fullmod.poly.A)
+#poly plotting stomtal conductence vs time per Drought
+visreg_A_D<-visreg(fullmod.poly.A, xvar="Year", by="Site.Lat", cond=list(Drought="D"),jitter=TRUE, gg=TRUE)+
+  facet_wrap(.~Site.Lat)+
+  theme(panel.background=element_rect(fill="white"), strip.background=element_rect(fill="white"),
+        panel.grid.major=element_line(colour="grey90"),
+        panel.grid.minor=element_line(colour="grey90"), 
+        axis.text.x=element_text(angle=45,hjust=1))
+visreg_A_D
+visreg_A_W<-visreg(fullmod.poly.A, xvar="Year", by="Site.Lat", cond=list(Drought="W"),jitter=TRUE, gg=TRUE)+
+  facet_wrap(.~Site.Lat)+
+  theme(panel.background=element_rect(fill="white"), strip.background=element_rect(fill="white"),
+        panel.grid.major=element_line(colour="grey90"),
+        panel.grid.minor=element_line(colour="grey90"), 
+        axis.text.x=element_text(angle=45,hjust=1))
+visreg_A_W
+
+
+
+
 ########################################################################################################################
 #Fitness associated traits
 #####Above Ground Biomass
@@ -178,6 +316,36 @@ Anova(noDxY.bio, type = 3) #Site X Drought, Site X Year
 visreg(noDxY.bio, xvar="Year", by="Site.Lat") #some variaiblity in biomass over time. 
 #Unclear how biologically meainingful it is.
 visreg(noDxY.bio, xvar="Year", by="Drought") #trend to less biomass over time, less bioamss in Drought
+
+#Poly 3-way
+fullmod.poly.bio <- lmer(Biomass ~ Site.Lat*poly(Year,2)*Drought + (1|Family) + (1|Block), data=y3)
+# drop 3way
+no3way.poly.bio <- lmer(Biomass ~ Site.Lat*Drought + Drought*poly(Year,2) + Site.Lat*poly(Year,2) 
+                       + (1|Family) + (1|Block), data=y3)
+lrtest(fullmod.poly.bio, no3way.poly.bio) # keep 3-way
+#compare poly to non-poly-3way
+lrtest(fullmod.poly.bio, fullmod.bio) #poly 3-way is better
+Anova(fullmod.poly.bio)
+#poly plotting stomtal conductence vs time per Drought
+visreg_bio_D<-visreg(fullmod.poly.bio, xvar="Year", by="Site.Lat", cond=list(Drought="D"),jitter=TRUE, gg=TRUE)+
+  facet_wrap(.~Site.Lat)+
+  theme(panel.background=element_rect(fill="white"), strip.background=element_rect(fill="white"),
+        panel.grid.major=element_line(colour="grey90"),
+        panel.grid.minor=element_line(colour="grey90"), 
+        axis.text.x=element_text(angle=45,hjust=1))
+visreg_bio_D
+visreg_bio_W<-visreg(fullmod.poly.bio, xvar="Year", by="Site.Lat", cond=list(Drought="W"),jitter=TRUE, gg=TRUE)+
+  facet_wrap(.~Site.Lat)+
+  theme(panel.background=element_rect(fill="white"), strip.background=element_rect(fill="white"),
+        panel.grid.major=element_line(colour="grey90"),
+        panel.grid.minor=element_line(colour="grey90"), 
+        axis.text.x=element_text(angle=45,hjust=1))
+visreg_bio_W
+
+
+
+
+
 
 ##### Flower_num #### #glmer did not converge. glmmTMB also did not converge
 #fullmod.num <- glmer(Flower_num ~ Site.Lat*Year*Drought + (1|Family) + (1|Block),
