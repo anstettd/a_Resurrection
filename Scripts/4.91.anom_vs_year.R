72bec84cd858bf486fb274d7754dd25d4c31751e
anstettd <daniel_anstett@yahoo.ca>
2019-12-09 15:57:59 -0800

    Paper 1 supl graphs

--- Scripts/4.91.anom_vs_year.R
+++ Scripts/4.91.anom_vs_year.R
@@ -1,5 +1,5 @@
 #################
-# Plot CMD.anom over time per year
+# CMDA, MATA, MAPA versus Year per site
 #################
 library(tidyverse)
 library(lsmeans)
@@ -16,51 +16,10 @@ library(lmtest)
 library(glmmTMB)
 library(MuMIn)
 library(Hmisc)
-y3 <- read.csv("Data/y3.csv", header=T) #Imports main dataset
+
 wna_anom <- read.csv("Data/wna_all.csv", header=T) #Imports climate dataset by site/year
 
-#CMD.weath versus Year
-weath.year <- ggplot(wna_anom, aes(Year,CMD.weath,color=Site))+
-  geom_point(size=2)+
-  geom_line()+
-  theme_minimal()
-#All in one Graph
-weath.year + theme(legend.text = element_text(size = 12, face = "bold"), 
-                   axis.text.x = element_text(size=14, face="bold", angle=0,hjust=0),
-                   axis.text.y = element_text(size=14,face="bold"),
-                   axis.title.x = element_text(color="black", size=16, vjust = 0.5, face="bold"),
-                   axis.title.y = element_text(color="black", size=16,vjust = 2, face="bold")) 
-weath.year.f <- weath.year + facet_wrap( ~ Site, ncol=4)
-weath.year.f + theme(legend.text = element_text(size = 12, face = "bold"),
-                     axis.text.x = element_text(size=14, face="bold", angle=45,hjust=1),
-                     axis.text.y = element_text(size=14,face="bold"),
-                     axis.title.x = element_text(color="black", size=16, vjust = 0.5, face="bold"),
-                     axis.title.y = element_text(color="black", size=16,vjust = 2, face="bold")) +
-  scale_x_continuous(name="Year") +
-  scale_y_continuous(name="Climatic Moisture Deficit")
-
-
-#CMD.anom versus Year, All in one Graph
-anom.year <- ggplot(wna_anom, aes(Year,CMD.anom,color=Site))+
-  geom_point(size=2)+
-  geom_line()+
-  theme_minimal()
-anom.year + theme(legend.text = element_text(size = 12, face = "bold"), 
-                                            axis.text.x = element_text(size=14, face="bold", angle=0,hjust=0),
-                                            axis.text.y = element_text(size=14,face="bold"),
-                                            axis.title.x = element_text(color="black", size=16, vjust = 0.5, face="bold"),
-                                            axis.title.y = element_text(color="black", size=16,vjust = 2, face="bold")) 
-anom.year.f <- anom.year + facet_wrap( ~ Site, ncol=4)
-anom.year.f + theme(legend.text = element_text(size = 12, face = "bold"),
-                                     axis.text.x = element_text(size=14, face="bold", angle=45,hjust=1),
-                                     axis.text.y = element_text(size=14,face="bold"),
-                                     axis.title.x = element_text(color="black", size=16, vjust = 0.5, face="bold"),
-                                     axis.title.y = element_text(color="black", size=16,vjust = 2, face="bold")) +
-  scale_x_continuous(name="Year") +
-  scale_y_continuous(name="Climatic Moisture Deficit Anomaly")
-
-
-#CMD.weath versus Year
+#CMDA versus Year
 weath.year <- ggplot(wna_anom, aes(Year,CMD.anom,color=Site))+
   geom_point(size=2)+
   geom_line()+
@@ -79,6 +38,49 @@ weath.year.f + theme(legend.text = element_text(size = 12, face = "bold"),
                      axis.title.y = element_text(color="black", size=16,vjust = 2, face="bold")) +
   scale_x_continuous(name="Year") +
   scale_y_continuous(name="Climatic Moisture Deficit Anomaly")
+
+
+
+#MATA versus Year
+weath.year <- ggplot(wna_anom, aes(Year,MAT.anom,color=Site))+
+  geom_point(size=2)+
+  geom_line()+
+  theme_minimal()
+#All in one Graph
+weath.year + theme(legend.text = element_text(size = 12, face = "bold"), 
+                   axis.text.x = element_text(size=14, face="bold", angle=0,hjust=0),
+                   axis.text.y = element_text(size=14,face="bold"),
+                   axis.title.x = element_text(color="black", size=16, vjust = 0.5, face="bold"),
+                   axis.title.y = element_text(color="black", size=16,vjust = 2, face="bold")) 
+weath.year.f <- weath.year + facet_wrap( ~ Site, ncol=4)
+weath.year.f + theme(legend.text = element_text(size = 12, face = "bold"),
+                     axis.text.x = element_text(size=14, face="bold", angle=45,hjust=1),
+                     axis.text.y = element_text(size=14,face="bold"),
+                     axis.title.x = element_text(color="black", size=16, vjust = 0.5, face="bold"),
+                     axis.title.y = element_text(color="black", size=16,vjust = 2, face="bold")) +
+  scale_x_continuous(name="Year") +
+  scale_y_continuous(name="Mean Annual Temp Anomaly°C")
+
+#MAPA versus Year
+weath.year <- ggplot(wna_anom, aes(Year,MAP.anom,color=Site))+
+  geom_point(size=2)+
+  geom_line()+
+  theme_minimal()
+#All in one Graph
+weath.year + theme(legend.text = element_text(size = 12, face = "bold"), 
+                   axis.text.x = element_text(size=14, face="bold", angle=0,hjust=0),
+                   axis.text.y = element_text(size=14,face="bold"),
+                   axis.title.x = element_text(color="black", size=16, vjust = 0.5, face="bold"),
+                   axis.title.y = element_text(color="black", size=16,vjust = 2, face="bold")) 
+weath.year.f <- weath.year + facet_wrap( ~ Site, ncol=4)
+weath.year.f + theme(legend.text = element_text(size = 12, face = "bold"),
+                     axis.text.x = element_text(size=14, face="bold", angle=45,hjust=1),
+                     axis.text.y = element_text(size=14,face="bold"),
+                     axis.title.x = element_text(color="black", size=16, vjust = 0.5, face="bold"),
+                     axis.title.y = element_text(color="black", size=16,vjust = 2, face="bold")) +
+  scale_x_continuous(name="Year") +
+  scale_y_continuous(name="log Mean Annual Precipitation Anomaly")
+
 
 
 
