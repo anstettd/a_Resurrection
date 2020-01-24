d14a5c18ee12f0bf046d9756775d9f4267c389d0
haleybranch <haley.branch@biodiversity.ubc.ca>
2020-01-24 15:53:58 -0800

    Slopes updated

--- Paper 2/P2 0.9_Slopes_Calc.R
+++ Paper 2/P2 0.9_Slopes_Calc.R
@@ -61,20 +61,20 @@ for (i in 1:12){
 colnames(slopes.rapid)[5]<-"SLA_Wet"
 
 #SLA_Dry
-vis_SLA_W<-visreg(fullmod.SLA, xvar="Year", by="Site.Lat", cond=list(Drought="D"))
-fit_SLA_W<-vis_SLA_W$fit
+vis_SLA_D<-visreg(fullmod.SLA, xvar="Year", by="Site.Lat", cond=list(Drought="D"))
+fit_SLA_D<-vis_SLA_D$fit
 for (i in 1:12){
-  fit_SLA_W_tmp<-fit_SLA_W %>% filter(Site.Lat==flower_dry_pop[i])
-  lm_SLA_W<-lm(visregFit~Year, data=fit_SLA_W_tmp)
-  summary_SLA_W<-summary(lm_SLA_W)
-  slopes.rapid[i,6]<-summary_SLA_W$coefficients[2,1]
+  fit_SLA_D_tmp<-fit_SLA_D %>% filter(Site.Lat==flower_dry_pop[i])
+  lm_SLA_D<-lm(visregFit~Year, data=fit_SLA_D_tmp)
+  summary_SLA_D<-summary(lm_SLA_D)
+  slopes.rapid[i,6]<-summary_SLA_D$coefficients[2,1]
 }
 colnames(slopes.rapid)[6]<-"SLA_Dry"
 
 #Stomatal Conductance_Dry
 fullmod.gs <- lmer(Stomatal_Conductance ~ Site.Lat*Year*Drought + (1|Family) + (1|Block), 
                    control=lmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=100000)), data=y5)
-vis_gs_D<-visreg(fullmod, xvar="Year", by="Site.Lat", cond=list(Drought="D"))
+vis_gs_D<-visreg(fullmod.gs, xvar="Year", by="Site.Lat", cond=list(Drought="D"))
 fit_gs_D<-vis_gs_D$fit
 for (i in 1:12){
   fit_gs_D_tmp<-fit_gs_D %>% filter(Site.Lat==flower_dry_pop[i])
@@ -85,84 +85,84 @@ for (i in 1:12){
 colnames(slopes.rapid)[7]<-"Stomatal_Conductance_Dry"
 
 #Gs Wet
-vis_gs_D<-visreg(Drought.gs, xvar="Year", by="Site.Lat", cond=list(Drought="W"))
-fit_gs_D<-vis_gs_D$fit
+vis_gs_W<-visreg(fullmod.gs, xvar="Year", by="Site.Lat", cond=list(Drought="W"))
+fit_gs_W<-vis_gs_W$fit
 for (i in 1:12){
-  fit_gs_D_tmp<-fit_gs_D %>% filter(Site.Lat==flower_dry_pop[i])
-  lm_gs_D<-lm(visregFit~Year, data=fit_gs_D_tmp)
-  summary_gs_D<-summary(lm_gs_D)
-  slopes.rapid[i,8]<-summary_gs_D$coefficients[2,1]
+  fit_gs_W_tmp<-fit_gs_W %>% filter(Site.Lat==flower_dry_pop[i])
+  lm_gs_W<-lm(visregFit~Year, data=fit_gs_W_tmp)
+  summary_gs_W<-summary(lm_gs_W)
+  slopes.rapid[i,8]<-summary_gs_W$coefficients[2,1]
 }
 colnames(slopes.rapid)[8]<-"Stomatal_Conductance_Wet"
 
 
 #WaterContent_Dry
 fullmod.wc <- lmer(Water_Content ~ Site.Lat*Year*Drought + (1|Family) + (1|Block), data=y5)
-vis_gs_D<-visreg(fullmod.wc, xvar="Year", by="Site.Lat", cond=list(Drought="D"))
-fit_gs_D<-vis_gs_D$fit
+vis_wc_D<-visreg(fullmod.wc, xvar="Year", by="Site.Lat", cond=list(Drought="D"))
+fit_wc_D<-vis_gs_D$fit
 for (i in 1:12){
-  fit_gs_D_tmp<-fit_gs_D %>% filter(Site.Lat==flower_dry_pop[i])
-  lm_gs_D<-lm(visregFit~Year, data=fit_gs_D_tmp)
-  summary_gs_D<-summary(lm_gs_D)
-  slopes.rapid[i,9]<-summary_gs_D$coefficients[2,1]
+  fit_wc_D_tmp<-fit_gs_D %>% filter(Site.Lat==flower_dry_pop[i])
+  lm_wc_D<-lm(visregFit~Year, data=fit_wc_D_tmp)
+  summary_wc_D<-summary(lm_wc_D)
+  slopes.rapid[i,9]<-summary_wc_D$coefficients[2,1]
 }
 colnames(slopes.rapid)[9]<-"Water_Content_Dry"
 
 #WaterContent_Wet
-fullmod.wc <- lmer(Water_Content ~ Site.Lat*Year*Drought + (1|Family) + (1|Block), data=y5)
-vis_gs_D<-visreg(fullmod.wc, xvar="Year", by="Site.Lat", cond=list(Drought="W"))
-fit_gs_D<-vis_gs_D$fit
+#fullmod.wc <- lmer(Water_Content ~ Site.Lat*Year*Drought + (1|Family) + (1|Block), data=y5)
+vis_wc_W<-visreg(fullmod.wc, xvar="Year", by="Site.Lat", cond=list(Drought="W"))
+fit_wc_W<-vis_wc_W$fit
 for (i in 1:12){
-  fit_gs_D_tmp<-fit_gs_D %>% filter(Site.Lat==flower_dry_pop[i])
-  lm_gs_D<-lm(visregFit~Year, data=fit_gs_D_tmp)
-  summary_gs_D<-summary(lm_gs_D)
-  slopes.rapid[i,10]<-summary_gs_D$coefficients[2,1]
+  fit_wc_W_tmp<-fit_gs_D %>% filter(Site.Lat==flower_dry_pop[i])
+  lm_wc_W<-lm(visregFit~Year, data=fit_wc_W_tmp)
+  summary_wc_D<-summary(lm_wc_W)
+  slopes.rapid[i,10]<-summary_wc_D$coefficients[2,1]
 }
 colnames(slopes.rapid)[10]<-"Water_Content_Wet"
 
 #Assimilaton_Dry
-fullmod.cmd.A <- lmer(Assimilation ~ Site.Lat*CMD.anom.s*Drought + (1|Family) + (1|Block) + (1|Year), 
-                      control=lmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=100000)), data=y3)
-vis_A_D<-visreg(fullmod.cmd.A, xvar="CMD.anom.s", by="Site.Lat", cond=list(Drought="D"))
+fullmod.cmd.A <- lmer(Assimilation ~ Site.Lat*Year*Drought + (1|Family) + (1|Block) + (1|Year), 
+                      control=lmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=100000)), data=y5)
+vis_A_D<-visreg(fullmod.cmd.A, xvar="Year", by="Site.Lat", cond=list(Drought="D"))
 fit_A_D<-vis_A_D$fit
 for (i in 1:12){
   fit_A_D_tmp<-fit_A_D %>% filter(Site.Lat==flower_dry_pop[i])
-  lm_A_D<-lm(visregFit~CMD.anom.s, data=fit_A_D_tmp)
+  lm_A_D<-lm(visregFit~Year, data=fit_A_D_tmp)
   summary_A_D<-summary(lm_A_D)
   slopes.rapid[i,11]<-summary_A_D$coefficients[2,1]
 }
 colnames(slopes.rapid)[11]<-"Assimilation_Dry"
 
 #Assimilation_Wet
-vis_A_W<-visreg(fullmod.cmd.A, xvar="CMD.anom.s", by="Site.Lat", cond=list(Drought="W"))
+vis_A_W<-visreg(fullmod.cmd.A, xvar="Year", by="Site.Lat", cond=list(Drought="W"))
 fit_A_W<-vis_A_W$fit
 for (i in 1:12){
   fit_A_W_tmp<-fit_A_W %>% filter(Site.Lat==flower_dry_pop[i])
-  lm_A_W<-lm(visregFit~CMD.anom.s, data=fit_A_W_tmp)
+  lm_A_W<-lm(visregFit~Year, data=fit_A_W_tmp)
   summary_A_W<-summary(lm_A_W)
   slopes.rapid[i,12]<-summary_A_W$coefficients[2,1]
 }
 colnames(slopes.rapid)[12]<-"Assimilation_Wet"
 
 #Biomass_Dry
-fullmod.cmd.bio <- lmer(Biomass ~ Site.Lat*CMD.anom.s*Drought + (1|Family) + (1|Block) + (1|Year), 
-                        control=lmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=100000)), data=y3)
-vis_bio_D<-visreg(fullmod.cmd.bio, xvar="CMD.anom.s", by="Site.Lat", cond=list(Drought="D"))
+fullmod.cmd.bio <- lmer(Biomass ~ Site.Lat*Year*Drought + (1|Family) + (1|Block) + (1|Year), 
+                        control=lmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=100000)), data=y5)
+vis_bio_D<-visreg(fullmod.cmd.bio, xvar="Year", by="Site.Lat", cond=list(Drought="D"))
 fit_bio_D<-vis_bio_D$fit
 for (i in 1:12){
   fit_bio_D_tmp<-fit_bio_D %>% filter(Site.Lat==flower_dry_pop[i])
-  lm_bio_D<-lm(visregFit~CMD.anom.s, data=fit_bio_D_tmp)
+  lm_bio_D<-lm(visregFit~Year, data=fit_bio_D_tmp)
   summary_bio_D<-summary(lm_bio_D)
   slopes.rapid[i,13]<-summary_bio_D$coefficients[2,1]
 }
 colnames(slopes.rapid)[13]<-"Biomass_Dry"
 
 #Biomass_Wet
-vis_bio_W<-visreg(fullmod.cmd.bio, xvar="CMD.anom.s", by="Site.Lat", cond=list(Drought="W"))
+vis_bio_W<-visreg(fullmod.cmd.bio, xvar="Year", by="Site.Lat", cond=list(Drought="W"))
 fit_bio_W<-vis_bio_W$fit
 for (i in 1:12){
   fit_bio_W_tmp<-fit_bio_W %>% filter(Site.Lat==flower_dry_pop[i])
-  lm_bio_W<-lm(visregFit~CMD.anom.s, data=fit_bio_W_tmp)
+  lm_bio_W<-lm(visregFit~Year, data=fit_bio_W_tmp)
   summary_bio_W<-summary(lm_bio_W)
   slopes.rapid[i,14]<-summary_bio_W$coefficients[2,1]
 }
