1e8cc52c4339e2164899ada1ab15d8a688879dd0
danstett <daniel_anstett@yahoo.ca>
2022-01-30 09:59:42 -0500

    PSRI Update

--- Scripts_Paper3/Old/1.3 climwin.R
+++ Scripts_Paper3/Old/1.3 climwin.R
@@ -17,18 +17,23 @@ y9 <- read.csv("Data/y9.csv", header=T) #Import trait data
 ## Wet, flowering date
 wflower <- y9 %>% filter(Drought=="W") %>% select(Year,Experiment_Date,Site) #Reformat
 wflower <- na.omit(wflower)
+spei9<-na.omit(spei9)
 
-
-MassWin <- slidingwin(xvar = list(Temp = spei_gather$Temp),
-                      cdate = spei_gather$Year,
+MassWin <- slidingwin(exclude=NA, xvar = list(SPEI = spei9$SPEI),
+                      cdate = spei9$Year,
                       bdate = wflower$Year,
-                      baseline = lm(Mass ~ 1, data = Mass),
-                      cinterval = "year",
-                      range = c(48, 0),
+                      baseline = lm(Experiment_Date ~ 1, data = wflower),
+                      cinterval = "day",
+                      range = c(3, 0),
                       type = "relative",
                       stat = "mean",
-                      func = "lin", spatial = list(y3w_flower$Site, spei_gather$SiteID))
+                      cmissing = FALSE,
+                      func = "lin", spatial = list(wflower$Site, spei9$Site))
 
+#Get info
+head(MassWin[[1]]$Dataset)
+MassWin[[1]]$BestModel
+head(MassWin[[1]]$BestModelData)
 
 
 
